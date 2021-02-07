20a1d9943e178be28fb4604fb22e02119268092a
Shahed70 <58668503+Shahed70@users.noreply.github.com>
2021-02-07 12:51:25 +0600

    Final commit of this assignment

--- js/main.js
+++ js/main.js
@@ -1,38 +1,50 @@
 const findMeal = ()=>{
+    document.getElementById('meal-container').innerHTML = ''
     const mealName = document.getElementById('meal-name').value;
     loadData(mealName)
-    document.getElementById('meal-name').value = ''
-    
+    document.getElementById('meal-name').value = '';  
 }
 
 const loadData=(mealName)=>{
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${mealName}`)
     .then(res => res.json())
     .then(data => getMealList(data.meals))
-    .catch(error => console.log(error))
+    .catch(error => alert(`Sorry the food name you entered is not matched`))
 }
 
 const getMealList = (mealLists)=> {
-    const mealsContainer = document.getElementById('meal-container')
-    mealLists.forEach(meal => {
-        const div = document.createElement('div')
-        div.className = 'my-div'
-        console.log(div);
-        const showMeals = `<a onclick="showDetails('${meal.strMeal}', '${meal.strMealThumb}')" href="#detailsId"> <img class="" src="${meal.strMealThumb}" alt=""> <h5>${meal.strMeal}</h5></a>`
-        mealsContainer.appendChild(div)
-        div.innerHTML = showMeals;
-        // location.reload();
+const mealsContainer = document.getElementById('meal-container')
+     mealLists.forEach(meal => {
+     const div = document.createElement('div')
+     div.className = 'my-div'
+     const showMeals = `<a onclick="showDetails('${meal.strMeal}', '${meal.strMealThumb}')" href="#detailsId"> <img class="" src="${meal.strMealThumb}" alt=""> <h5>${meal.strMeal}</h5></a>`
+     mealsContainer.appendChild(div)
+     div.innerHTML = showMeals;
+
     });
 };
 
 const showDetails = (details, image) => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${details}`)
+     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${details}`)
     .then(res => res.json())
     .then(data => getDetails(data.meals, image))
 }
 
 const getDetails = (detail, image) => {
-    const detailsContainer = document.getElementById('details-container')
-    const detailsData = `<div id="detailsId"> <img class="" src="${image}" alt="${detail[0].strMeal}"> <div> <h3>${detail[0].strMeal}</h3><h5>Ingradients</h5> <p><i class="fa fa-check"></i>${detail[0].strIngredient1}</p> <p><i class="fa fa-check"></i>${detail[0].strIngredient2}</p> <p><i class="fa fa-check"></i>${detail[0].strIngredient3}</p> <p><i class="fa fa-check"></i>${detail[0].strIngredient4}</p> <p><i class="fa fa-check"></i>${detail[0].strIngredient5}</p> <p><i class="fa fa-check"></i>${detail[0].strIngredient6}</p> <p><i class="fa fa-check"></i>${detail[0].strIngredient7}</p> <p><i class="fa fa-check"></i>${detail[0].strIngredient8}</p></div></div>` 
+const detailsContainer = document.getElementById('details-container')
+const detailsData = `
+    <div id="detailsId"> <img class="" src="${image}" alt="${detail[0].strMeal}">
+    <div> <h3>${detail[0].strMeal}</h3>
+    <h5>Ingradients</h5> 
+    <p><i class="fa fa-check"></i>${detail[0].strIngredient1}</p>
+    <p><i class="fa fa-check"></i>${detail[0].strIngredient2}</p>
+    <p><i class="fa fa-check"></i>${detail[0].strIngredient3}</p>
+    <p><i class="fa fa-check"></i>${detail[0].strIngredient4}</p> 
+    <p><i class="fa fa-check"></i>${detail[0].strIngredient5}</p> 
+    <p><i class="fa fa-check"></i>${detail[0].strIngredient6}</p> 
+    <p><i class="fa fa-check"></i>${detail[0].strIngredient7}</p> 
+    <p><i class="fa fa-check"></i>${detail[0].strIngredient8}</p>
+    </div>
+    </div>` 
     detailsContainer.innerHTML = detailsData;
 }
